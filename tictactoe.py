23daa9f2d8ad0560c60ea599bd5472a88cc39b64
Nurul Huda Munna <nurulhudamunna@Munnas-MacBook-Air.local>
2024-06-16 22:55:43 +0600

    choosing player X or O

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 # Tic_Tac_Toe
 import random
+
 # Printing the Board on the Screen
 def drawBoard(board):
     # This function prints out the board that it was passed.
@@ -9,3 +10,17 @@ def drawBoard(board):
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
+    
+# Letting the Player Choose X or O
+def inputPlayerLetter():
+    # Let the player enter which letter they want to be.
+    # Returns a list with the player's letter as the first item and the computer's letter as the second.
+    letter = ''
+    while not(letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+    # The first element in the list is the player's letter; the second is the computer's letter.
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
